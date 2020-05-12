def9dcd5893cf85be13ec0e8d2aea24552c81952
ornwipa <fahornwipa@gmail.com>
2020-05-12 15:19:09 -0600

    build ngram document

--- Python Codes/Complete Code.py
+++ Python Codes/Complete Code.py
@@ -7,18 +7,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from DictionarySetup import termsIndustrialEngineering
 from ScoresCalculation import themeIndustrialEngineering
+from sklearn.feature_extraction.text import CountVectorizer
 
 text = textract.process('../Files/ornwipa_resume.pdf', encoding='ascii')
 text = str(text.lower())
 text = text.replace('\n', ' ')
 text = text.replace('\\n', ' ')
 text = text.translate(str.maketrans('','',string.punctuation))
+text = re.sub(' +', ' ', text)
+
+vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 2))
+vectorizer.fit_transform([text])
+ngram_text = vectorizer.get_feature_names()
 
 termsDict = termsIndustrialEngineering()
 terms = termsDict.getTerms()
 
 scoresList = themeIndustrialEngineering()
-scores = scoresList.countScore(terms, text)
+scores = scoresList.countScore(terms, ngram_text)[0]
+terms_included = scoresList.countScore(terms, ngram_text)[1]
+print(terms_included)
         
 # Create a data frame with the scores summary
 summary = pd.DataFrame(scores,index=terms.keys(),columns=['score']).sort_values(by='score',ascending=False)
--- Python Codes/ScoresCalculation.py
+++ Python Codes/ScoresCalculation.py
@@ -9,10 +9,11 @@ class themeIndustrialEngineering:
         self.data = 0
         self.healthcare = 0
 
+    def countScore(self, terms, text):
         # Create an empty list where the scores will be stored
         self.scores = []
-
-    def countScore(self, terms, text):
+        self.terms_included = []
+        
         # Obtain the scores for each area
         for area in terms.keys():
         
@@ -20,35 +21,41 @@ class themeIndustrialEngineering:
                 for word in terms[area]:
                     if word in text:
                         self.quality +=1
+                        self.terms_included.append(word)
                 self.scores.append(self.quality)
         
             elif area == 'Operations management':
                 for word in terms[area]:
                     if word in text:
                         self.operations +=1
+                        self.terms_included.append(word)
                 self.scores.append(self.operations)
         
             elif area == 'Supply chain':
                 for word in terms[area]:
                     if word in text:
                         self.supplychain +=1
+                        self.terms_included.append(word)
                 self.scores.append(self.supplychain)
         
             elif area == 'Project management':
                 for word in terms[area]:
                     if word in text:
                         self.project +=1
+                        self.terms_included.append(word)
                 self.scores.append(self.project)
         
             elif area == 'Data analytics':
                 for word in terms[area]:
                     if word in text:
                         self.data +=1
+                        self.terms_included.append(word)
                 self.scores.append(self.data)
         
             else:
                 for word in terms[area]:
                     if word in text:
                         self.healthcare +=1
+                        self.terms_included.append(word)
                 self.scores.append(self.healthcare)
-        return self.scores
+        return (self.scores, self.terms_included)
